561f641e05a9745613780e6f5da3370ee2ff5bac
Denis Sanchez Leyva <denisijcu266@gmail.com>
2026-01-11 06:42:41 -0500

    Fix import order for passport-jwt in jwt.strategy.ts

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,6 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
+//import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Strategy, ExtractJwt } from 'passport-jwt';
 import { UserService } from './user.service';
 
 @Injectable()
