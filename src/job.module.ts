aa5f01fee739d015b43027ac6a8394349f8be43d
Denis Sanchez Leyva <denisijcu266@gmail.com>
2026-01-11 07:55:02 -0500

    Update job.module.ts

--- src/job.module.ts
+++ src/job.module.ts
@@ -1,14 +1,20 @@
-import { Module, forwardRef } from '@nestjs/common'; // Agregamos forwardRef por seguridad
+import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserService } from './auth/user.service';
-import { User, UserSchema } from './user.schema';
-import { AuthModule } from './auth/auth.module'; 
+import { JobResolver } from './job.resolver';
+import { JobService } from './auth/job.service';
+import { Job, JobSchema } from './job.schema';
+import { Notification, NotificationSchema } from './notification.schema';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user.module';
+import { NotificationService } from './auth/notification.service';
+import { ChatModule } from './chat.module';
 
 @Module({
   imports: [
     AuthModule, 
     UserModule, 
     ChatModule,
+    // Definimos los modelos para que este módulo los use
     MongooseModule.forFeature([
       { name: Job.name, schema: JobSchema },
       { name: Notification.name, schema: NotificationSchema },
@@ -22,7 +28,7 @@ import { AuthModule } from './auth/auth.module';
   exports: [
     JobService, 
     NotificationService, 
-    MongooseModule // Vital para que AppResolver vea los modelos
+    MongooseModule // <--- Esto permite que el AppResolver vea el JobModel
   ], 
 })
-export class JobModule {} // 👈 ASEGÚRATE DE QUE TENGA EL 'export'
+export class JobModule {}
