05849f18301d0a57f0e38bf75389741b906cdc4e
Denis Sanchez Leyva <denisijcu266@gmail.com>
2026-01-11 05:12:43 -0500

    Add optional fields and UserBasicType class

--- src/job.type.ts
+++ src/job.type.ts
@@ -16,4 +16,38 @@ export class JobType {
 
   @Field()
   price: number;
+
+  @Field({ nullable: true })
+  location?: string;
+
+  @Field({ nullable: true })
+  category?: string;
+
+  @Field(() => UserBasicType, { nullable: true })
+  client?: UserBasicType;
+
+  @Field(() => UserBasicType, { nullable: true })
+  provider?: UserBasicType;
+
+  @Field({ nullable: true })
+  createdAt?: string;
+
+  @Field({ nullable: true })
+  updatedAt?: string;
+}
+
+// Crear este tipo para evitar referencias circulares
+@ObjectType()
+export class UserBasicType {
+  @Field(() => ID)
+  _id: string;
+
+  @Field()
+  name: string;
+
+  @Field()
+  email: string;
+
+  @Field({ nullable: true })
+  avatar?: string;
 }
