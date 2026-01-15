d7cf1609847af24afa046106d69263a5e43fd40c
Denis Sanchez Leyva <denisijcu266@gmail.com>
2026-01-15 00:56:30 -0500

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -37,10 +37,6 @@ import { Job, JobSchema } from './job.schema';
     }),
 
     GraphQLModule.forRoot<ApolloDriverConfig>({
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> d183c33554e4fb6cd7d5f5d07daf0936c8f0dd37
       driver: ApolloDriver,
       csrfPrevention: false, // Mantener en false para compatibilidad con Apollo Sandbox y uploads
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
@@ -65,28 +61,6 @@ import { Job, JobSchema } from './job.schema';
         };
       },
     }),
-<<<<<<< HEAD
-=======
-  driver: ApolloDriver,
-  csrfPrevention: false, // Permite peticiones sin headers especiales
-  autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-  sortSchema: true,
-  playground: true,      // Forzamos visualización del playground en Render
-  introspection: true,   // Forzamos lectura del esquema desde fuera
-  context: ({ req, res }: { req: any; res: any }) => ({ req, res }),
-  formatError: (error) => {
-    // Esto te ayudará a debuguear errores en la consola de Render
-    console.error('❌ GraphQL Error:', error);
-    return {
-      message: error.message,
-      code: error.extensions?.code,
-      path: error.path,
-    };
-  },
-}),
->>>>>>> 6bf5987400caab120e289927937bb7636d667405
-=======
->>>>>>> d183c33554e4fb6cd7d5f5d07daf0936c8f0dd37
 
     MongooseModule.forRoot(
       process.env.MONGODB_URI || 'mongodb://localhost:27017/vertex-coders-db',
